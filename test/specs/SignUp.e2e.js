7624057c7819096cba3e9aa467a1415787249daf
Lumenrv <pavlenkoillai@gmail.com>
2022-10-14 13:46:05 +0300

    update

--- test/specs/SignUp.e2e.js
+++ test/specs/SignUp.e2e.js
@@ -8,7 +8,7 @@ describe("SignUp page test", () => {
     await Page.openMainPageAndAcceptCoockies();
     await SignUpPage.openSignUpPage();
     await SignUpPage.enterRegDataAndSubmit();
-    await SignUpPage.verifyErorrMsg("reCAPTCHA validation required") // "reCAPTCHA validation required"
+    await SignUpPage.verifyErorrMsg("recaptcha") // "reCAPTCHA validation required"
   });
 
   it("Test sign Up with incorrect email", async () => {
